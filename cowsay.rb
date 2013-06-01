86eaddd92b0542d4a600a43a6d57b5e2446d867a
Johan Wirén <johan.wiren.se@gmail.com>
2013-06-01 23:06:34 +0200

    Sets content-type and added example redirect

--- cowsay.rb
+++ cowsay.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 require 'cowsay'
 
+before do
+    content_type 'text/plain'
+end
+
+get '/' do
+    redirect '/this%20is%20an%20example'
+end
+
 get '/:message' do
     Cowsay.say(params[:message])
 end
