7b8681e76473825911a8d980e4483a9893c4491a
Johan Wirén <johan.wiren.se@gmail.com>
2013-08-08 23:55:56 +0200

    Better help messages

--- cowsay.rb
+++ cowsay.rb
@@ -1,22 +1,32 @@
 require 'cowsay'
 require 'sinatra'
 
+def available_cows
+    (Cowsay::Character.constants - [:Base]).sort
+end
+
 before do
     content_type 'text/plain'
 end
 
 get '/' do
-    "Usage: http://#{request.env["HTTP_HOST"]}/message\n"
+    msg = "Usage: http://#{request.env["HTTP_HOST"]}/<character>/message\n\n"
+    msg += "Available characters: "
+    msg += available_cows.join(" ")
+    msg
 end
 
 get '/*/*' do
     cow = params[:splat][0].intern
-    cows = Cowsay::Character.constants - [:Base]
-    if cows.include? cow
+    if available_cows.include? cow
         message = params[:splat][1]
-        c = Cowsay::Character.const_get(cow).new
-        c.say(message)
-    end 
+    else
+        message = "Unknown character '#{cow}'. Available characters: "
+        message += available_cows.join(" ")
+        cow = 'Cow'
+    end
+    c = Cowsay::Character.const_get(cow).new
+    c.say(message)
 end
 
 get '/:message' do
